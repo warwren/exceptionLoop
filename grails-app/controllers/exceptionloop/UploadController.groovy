3eb1bc2ab724da8631f231be7a9d55e289bb73ef
Warren Wong <warren@spiekerpoint.com>
2018-05-09 23:35:38 -0600

    Added save action in case user didn't upload a large enough file to trigger the problem.

--- grails-app/controllers/exceptionloop/UploadController.groovy
+++ grails-app/controllers/exceptionloop/UploadController.groovy
@@ -3,4 +3,9 @@ package exceptionloop
 class UploadController {
 
     def index() { }
+
+    def save() {
+        log.error("Upload succeeded. Upload a file larger than 128k to trigger the problem.")
+        redirect(action: 'index')
+    }
 }
